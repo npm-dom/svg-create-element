0c44989390b3e15974b2d9ba18310069713e809b
James Halliday <mail@substack.net>
2014-08-03 15:48:41 +1200

    hyphenate

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ module.exports = function (name, attr) {
     var elem = document.createElementNS('http://www.w3.org/2000/svg', name);
     if (!attr) return elem;
     for (var key in attr) {
-        if (has(attr, key)) elem.setAttribute(key, attr[key]);
+        if (!has(attr, key)) continue;
+        var nkey = key.replace(/([a-z])([A-Z])/g, function (_, a, b) {
+            return a + '-' + b.toLowerCase();
+        });
+        elem.setAttribute(nkey, attr[key]);
     }
     return elem;
 }
